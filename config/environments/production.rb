3689f481613595c89e0dda3a5a904017440b08bf
imranhayat <imran.hayat92@gmail.com>
2021-05-07 23:52:42 +0500

    CSS fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, s-maxage=31536000, max-age=15552000',
+    'Expires' => 1.year.from_now.to_formatted_s(:rfc822)
+  }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
